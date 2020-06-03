859f82e40df2a0eff5c88a57c3552392697f384f
tyushnyakov <60808532+tyushnyakov@users.noreply.github.com>
2020-06-03 19:59:24 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+//FRONT CONTROLLER
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//1.Общие настройки
+
+//2.Подключение файлов системы
+define('ROOT', dirname(__FILE__));
+require_once(ROOT.'/components/Router.php');
+require_once(ROOT.'/components/Db.php');
+
+ 
+//3.Установка соединения с БД
+
+//4.Call ROUTER
+$router = new Router();
+$router->run();
+
+echo 'test';
+   ?>
