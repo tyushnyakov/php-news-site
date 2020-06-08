ae3b41a817e089ce2ef4bf6fea9cbc289435bfe6
tyushnyakov <60808532+tyushnyakov@users.noreply.github.com>
2020-06-08 17:10:45 +0300

    Add files via upload

---
+++ NewsController.php
@@ -0,0 +1,34 @@
+<?php
+
+include_once ROOT.'/models/News.php';
+
+class NewsController 
+{
+    public function actionIndex() 
+    {
+        $newsList = array();
+        $newsList = News::getNewsList();
+
+        require_once(ROOT.'/views/index.php');
+
+        echo '<pre>';
+        print_r($newsList);
+        echo '</pre>';
+
+        return true;
+    }
+
+    public function actionView($id) 
+    {
+        if($id) {
+            $newsItem = News::getNewsItemById($id);
+            echo '<pre>';
+            print_r($newsItem);
+            echo '</pre>';
+
+            echo 'actionView';
+        }
+        
+        
+    }
+}
